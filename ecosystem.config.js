ceb84a51979856562615faf45e4b4445c97f3bac
eletroswing <ytalosilvabatalha@gmail.com>
2024-02-02 21:25:36 -0300

    removing tests

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,41 +5,13 @@ module.exports = {
         script: './dist/src/index.js',
         env: {
           PORT: 3000,
-          CLERK_SECRET_KEY: '',
-          MONGO_URL: '',
-          NODE_ENV: 'DEVELOPMENT',
+          MONGO_URL: 'mongodb+srv://guardiao3301:uPgvdRyU2RxF1hy6@cluster0.nxhcsas.mongodb.net/db?retryWrites=true&w=majority',
+          NODE_ENV: 'PRODUCTION',
+          REDIS: 'redis://default:amjNR7LYEKjLbxGr1TUL8PLWq0yHMJCc@redis-13009.c289.us-west-1-2.ec2.cloud.redislabs.com:13009',
+          CLERK_SECRET_KEY: 'sk_test_vLcwR2JhrsUfUxHA88Gn9uI92mmLb4zt8J7qmPtdsR',
+          OPENAI_API_KEY: ''
         }
-      },
-      {
-        name: 'app2',
-        script: './dist/src/index.js',
-        env: {
-          PORT: 3001,
-          CLERK_SECRET_KEY: '',
-          MONGO_URL: '',
-          NODE_ENV: 'DEVELOPMENT',
-        }
-      },
-      {
-        name: 'app3',
-        script: './dist/src/index.js',
-        env: {
-          PORT: 3002,
-          CLERK_SECRET_KEY: '',
-          MONGO_URL: '',
-          NODE_ENV: 'DEVELOPMENT',
-        }
-      },
-      {
-        name: 'app4',
-        script: './dist/src/index.js',
-        env: {
-          PORT: 3004,
-          CLERK_SECRET_KEY: '',
-          MONGO_URL: '',
-          NODE_ENV: 'DEVELOPMENT',
-        }
-      },
+      }
     ]
   };
   
